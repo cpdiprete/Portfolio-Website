6270f47ac149674d641120da98aaed18caabd654
Calvin DiPrete <calvindiprete@lawn-128-61-45-55.lawn.gatech.edu>
2025-02-03 11:48:07 -0500

    new resume pdf link

--- src/Components/ResumeLink.jsx
+++ src/Components/ResumeLink.jsx
@@ -3,7 +3,7 @@ const ResumeLink = () => {
     return (
         <div className="flex justify-center mt-12 lg:mt-16">
             <a 
-                href="https://www.dropbox.com/scl/fi/1ckpjsx9wz5q1sikxq3qm/Calvin-DiPrete_Resume.pdf?rlkey=fyfaofzn4v3pf2hszvxj53mpl&dl=0" 
+                href="https://drive.google.com/file/d/1aY63CyM-ck6PApfWYX_wLUByorgDU-d7/view?usp=sharing" 
                 target="_blank" 
                 rel="noopener noreferrer" 
                 className="ml-1 inline-block rounded-xl border border-neutral-800 bg-purple-300 px-20 py-4 rounded-md bg-gradient-to-r from-purple-300 via-slate-500 
